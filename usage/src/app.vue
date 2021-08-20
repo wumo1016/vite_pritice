4dd6be52cd25570b7da3b5a453a85932f41c4680
wumo1016 <760478279@qq.com>
2021-08-20 21:26:00 +0800

    chore(usage)

--- usage/src/app.vue
+++ usage/src/app.vue
@@ -3,16 +3,31 @@
   <Helloword msg="abc" />
   <div>{{ state.count }}</div>
   <div><button @click="handleClick">+</button></div>
+  <div class="img_box"></div>
+  <!-- <img src="./assets/hook.jpg" alt=""> -->
+  <img :src="img" alt="" />
 </template>
 
 <script setup>
 import { reactive } from 'vue'
 import Helloword from './helloWord.vue'
+import hook from './assets/hook.jpg'
 
 let state = reactive({
   count: 1
 })
+
 const handleClick = () => {
   state.count++
 }
+
+const img = hook
 </script>
+
+<style scoped>
+.img_box {
+  width: 200px;
+  height: 200px;
+  background: url('./assets/hook.jpg') center / contain;
+}
+</style>
