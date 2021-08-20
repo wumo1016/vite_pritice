7722ac9ec9d0d45e25b2171f0935288b1f317fbc
wumo1016 <760478279@qq.com>
2021-08-20 20:31:34 +0800

    chore(usage)

---
+++ usage/src/app.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>app</div>
+  <Helloword msg="abc" />
+  <div>{{ state.count }}</div>
+  <div><button @click="handleClick">+</button></div>
+</template>
+
+<script setup>
+import { reactive } from 'vue'
+import Helloword from './helloWord.vue'
+
+let state = reactive({
+  count: 1
+})
+const handleClick = () => {
+  state.count++
+}
+</script>
---
+++ usage/src/helloWord.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>helloword</div>
+  <div>{{ msg }}</div>
+</template>
+
+<script setup>
+import { defineProps } from 'vue'
+defineProps({
+  msg: String
+})
+</script>
